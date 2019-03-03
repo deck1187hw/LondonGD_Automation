f3a879230cf6fea14a68337b75039b183cd67e58
Miguel Puig <miguelpuig@Miguels-Mac-Pro.local>
2019-03-03 13:11:30 +0000

    changes

--- gdscraper/gdscraper/spiders/ehafixtures.py
+++ gdscraper/gdscraper/spiders/ehafixtures.py
@@ -2,23 +2,32 @@
 import scrapy
 from gdscraper.items import EhafixturesItem
 
+
 class EhafixturesSpider(scrapy.Spider):
     name = 'ehafixtures'
     allowed_domains = ['englandhandball.com']
     start_urls = ['https://www.englandhandball.com/league/premier-handball-league']
     type = ''
     teamId = ''
+    typeF = ''
+    teamIdF = ''
 
-    def __init__(self, type='', teamId='',  *args, **kwargs):
+    def __init__(self, type='', teamId='', *args, **kwargs):
         self.type = type
         self.teamId = teamId
 
     def parse(self, response):
+        if "teamId" in response.meta:
+            self.typeF = response.meta["type"]
+            self.teamIdF = response.meta["teamId"]
+        else:
+            self.typeF = self.type
+            self.teamIdF = self.teamId
+
         leagueName = response.css('div.page-title div.container h1::text').extract_first()
 
-
         # Only for PHL Women take the 2nd
-        if self.type == "women" and "premier" in response.request.url:
+        if self.typeF == "women" and "premier" in response.request.url:
             leagueHtml = response.selector.xpath('//*[@id="standings"]/div[2]/div[2]').get()
             leagueName = response.selector.xpath('//*[@id="standings"]/div[2]/div[1]/p').get()
         else:
@@ -26,8 +35,7 @@ class EhafixturesSpider(scrapy.Spider):
             leagueName = response.selector.xpath('//*[@id="standings"]/div[1]/div[1]/p').get()
 
         item = EhafixturesItem()
-        item['itemNameLeague'] = response.request.url
+        item['itemNameLeague'] = leagueName
         item['itemHtml'] = leagueHtml
-        item['itemTeamId'] = self.teamId
+        item['itemTeamId'] = self.teamIdF
         return item
-
